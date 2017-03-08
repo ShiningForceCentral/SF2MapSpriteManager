34df6eccaa254b9e3a34c57c20e47cfd7c98cb40
Wiz <the_wizard_who_says_ni@hotmail.com>
2017-03-09 00:56:19 +0100

    Implemented disassembly and PNG import/export.

---
+++ src/com/sfc/sf2/mapsprite/MapSpriteManager.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sfc.sf2.mapsprite;
+
+import com.sfc.sf2.graphics.GraphicsManager;
+import com.sfc.sf2.graphics.Tile;
+import com.sfc.sf2.mapsprite.io.DisassemblyManager;
+import com.sfc.sf2.mapsprite.io.PngManager;
+import com.sfc.sf2.palette.PaletteManager;
+import java.awt.Color;
+
+/**
+ *
+ * @author wiz
+ */
+public class MapSpriteManager {
+       
+    private PaletteManager paletteManager = new PaletteManager();
+    private GraphicsManager graphicsManager = new GraphicsManager();
+    private Tile[] tiles;
+    private MapSprite[] mapSprites;
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(Tile[] tiles) {
+        this.tiles = tiles;
+    }
+       
+    public void importDisassembly(String paletteFilePath, String graphicsBasepath){
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.importDisassembly() - Importing disassembly ...");
+        paletteManager.importDisassembly(paletteFilePath);
+        Color[] palette = paletteManager.getPalette();
+        palette[0] = new Color(255, 255, 255, 0);
+        mapSprites = DisassemblyManager.importDisassembly(graphicsBasepath, palette);
+        tiles = new Tile[mapSprites.length*18];
+        for(int i=0;i<mapSprites.length;i++){
+            System.arraycopy(mapSprites[i].getTiles(), 0, tiles, i*18, 18);
+        }
+        graphicsManager.setTiles(tiles);
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.importDisassembly() - Disassembly imported.");
+    }
+    
+    public void exportDisassembly(String basepath){
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.importDisassembly() - Exporting disassembly ...");
+        DisassemblyManager.exportDisassembly(mapSprites, basepath);
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.importDisassembly() - Disassembly exported.");        
+    }   
+    
+    public void importRom(String romFilePath, String paletteOffset, String paletteLength, String graphicsOffset, String graphicsLength){
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.importOriginalRom() - Importing original ROM ...");
+        graphicsManager.importRom(romFilePath, paletteOffset, paletteLength, graphicsOffset, graphicsLength,GraphicsManager.COMPRESSION_BASIC);
+        tiles = graphicsManager.getTiles();
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.importOriginalRom() - Original ROM imported.");
+    }
+    
+    public void exportRom(String originalRomFilePath, String graphicsOffset){
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.exportOriginalRom() - Exporting original ROM ...");
+        graphicsManager.exportRom(originalRomFilePath, graphicsOffset, GraphicsManager.COMPRESSION_BASIC);
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.exportOriginalRom() - Original ROM exported.");        
+    }      
+    
+    public void importPng(String basepath){
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.importPng() - Importing PNG ...");
+        mapSprites = PngManager.importPng(basepath);
+        tiles = new Tile[mapSprites.length*18];
+        for(int i=0;i<mapSprites.length;i++){
+            System.arraycopy(mapSprites[i].getTiles(), 0, tiles, i*18, 18);
+        }
+        graphicsManager.setTiles(tiles);
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.importPng() - PNG imported.");
+    }
+    
+    public void exportPng(String basepath){
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.exportPng() - Exporting PNG ...");
+        PngManager.exportPng(mapSprites, basepath);
+        System.out.println("com.sfc.sf2.mapsprite.MapSpriteManager.exportPng() - PNG exported.");       
+    }
+}
---
+++ src/com/sfc/sf2/mapsprite/io/DisassemblyManager.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sfc.sf2.mapsprite.io;
+
+import com.sfc.sf2.graphics.GraphicsManager;
+import com.sfc.sf2.graphics.Tile;
+import com.sfc.sf2.graphics.compressed.BasicGraphicsDecoder;
+import com.sfc.sf2.graphics.compressed.BasicGraphicsEncoder;
+import com.sfc.sf2.graphics.uncompressed.UncompressedGraphicsDecoder;
+import com.sfc.sf2.graphics.uncompressed.UncompressedGraphicsEncoder;
+import com.sfc.sf2.mapsprite.MapSprite;
+import static com.sfc.sf2.mapsprite.io.PngManager.writePngFile;
+import java.awt.Color;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author wiz
+ */
+public class DisassemblyManager {
+
+    private static final String BASE_FILENAME = "mapspriteXXX.bin";
+    
+    public static MapSprite[] importDisassembly(String basepath, Color[] palette){
+        System.out.println("com.sfc.sf2.mapsprite.io.DisassemblyManager.importDisassembly() - Importing disassembly ...");
+        List<MapSprite> mapSprites = new ArrayList();
+        try{
+            for(int i=0;i<1000;i++){
+                String index = String.format("%03d", i);
+                String filePath = basepath + BASE_FILENAME.replace("XXX.bin", index+".bin");
+                Tile[] tiles = parseGraphics(filePath, palette);
+                if(tiles!=null){
+                    if(tiles.length==18){
+                       MapSprite mapSprite = new MapSprite();
+                       mapSprite.setIndex(i);                   
+                       mapSprite.setTiles(tiles);
+                       mapSprites.add(mapSprite);
+                       System.out.println("Created MapSprite " + i + " with " + tiles.length + " tiles.");                       
+                    }else{
+                        System.out.println("Could not create MapSprite " + i + " because of wrong length : tiles=" + tiles.length);
+                    }
+                }
+            }
+        }catch(Exception e){
+             System.err.println("com.sfc.sf2.mapsprite.io.PngManager.importPng() - Error while parsing graphics data : "+e);
+        }         
+                
+        System.out.println("com.sfc.sf2.mapsprite.io.DisassemblyManager.importDisassembly() - Disassembly imported.");
+        return mapSprites.toArray(new MapSprite[mapSprites.size()]);
+    }
+    
+    public static void exportDisassembly(MapSprite[] mapSprites, String basepath){
+        System.out.println("com.sfc.sf2.mapsprite.io.DisassemblyManager.exportDisassembly() - Exporting disassembly ...");
+        try {
+            for(MapSprite mapSprite : mapSprites){
+                String index = String.format("%03d", mapSprite.getIndex());
+                String filePath = basepath + System.getProperty("file.separator") + BASE_FILENAME.replace("XXX.bin", index+".bin");
+                BasicGraphicsEncoder.produceGraphics(mapSprite.getTiles());
+                byte[] newGraphicsFileBytes = BasicGraphicsEncoder.getNewGraphicsFileBytes(); 
+                Path graphicsFilePath = Paths.get(filePath);
+                Files.write(graphicsFilePath,newGraphicsFileBytes);
+                System.out.println(newGraphicsFileBytes.length + " bytes into " + graphicsFilePath);
+                System.out.println("com.sfc.sf2.mapsprite.io.DisassemblyManager.writeFiles() - File written.");                
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(DisassemblyManager.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            System.out.println(ex);
+        }            
+        System.out.println("com.sfc.sf2.mapsprite.io.DisassemblyManager.exportDisassembly() - Disassembly exported.");        
+    }    
+    
+    private static Tile[] parseGraphics(String filePath, Color[] palette){
+        Tile[] tiles = null;       
+        try{
+            Path path = Paths.get(filePath);
+            if(path.toFile().exists()){
+                byte[] data = Files.readAllBytes(path);
+                tiles = BasicGraphicsDecoder.decodeBasicGraphics(data, palette);
+            }            
+        }catch(Exception e){
+             System.err.println("com.sfc.sf2.mapsprite.io.DisassemblyManager.parseGraphics() - Error while parsing graphics data : "+e);
+        } 
+        return tiles;
+    }   
+
+    
+}
